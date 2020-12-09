2bfafa576b2c46310ec8c0e9eb8a8060e27689df
Wesley <weselykhma@gmail.com>
2020-12-09 02:07:38 -0500

    use S3 to serve files on prod mode

--- place/server.js
+++ place/server.js
@@ -1,65 +1,71 @@
+// Web server
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8081 });
- 
-var dim = 250; // note: this is not the right dimensions!!
-var board=new Array(dim);
-for(var x=0;x<dim;x++){
-	board[x]=new Array(dim);
-	for(var y=0;y<dim;y++){
-		board[x][y]={ 'r':255, 'g':255, 'b':255 };
+var express = require('express');
+var app = express();
+
+const SOCKET_PORT = 8081
+const HTTP_PORT = 8080
+const wss = new WebSocket.Server({ port: SOCKET_PORT });
+
+var DIM = 1000;
+var board = new Array(DIM);
+for (var x = 0; x < DIM; x++) {
+	board[x] = new Array(DIM);
+	for (var y = 0; y < DIM; y++) {
+		board[x][y] = { 'r': 255, 'g': 255, 'b': 255 };
 	}
 }
 
-wss.on('close', function() {
-    console.log('disconnected');
+wss.on('close', function () {
+	console.log('disconnected');
 });
 
 wss.broadcast = function broadcast(data) {
-  wss.clients.forEach(function each(client) {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(data);
-    }
-  });
+	wss.clients.forEach(function each(client) {
+		if (client.readyState === WebSocket.OPEN) {
+			client.send(data);
+		}
+	});
 };
 
 // for heartbeat to make sure connection is alive 
-function noop() {}
+function noop() { }
 function heartbeat() {
-  this.isAlive = true;
+	this.isAlive = true;
 }
 
-function isValidSet(o){
-	var isValid=false;
+function isValidSet(o) {
+	var isValid = false;
 	try {
-	   isValid = 
-		Number.isInteger(o.x) && o.x!=null && 0<=o.x && o.x<dim &&
-		Number.isInteger(o.y) && o.y!=null && 0<=o.y && o.y<dim && 
-		Number.isInteger(o.r) && o.r!=null && 0<=o.r && o.r<=255 && 
-		Number.isInteger(o.g) && o.g!=null && 0<=o.g && o.g<=255 && 
-		Number.isInteger(o.b) && o.b!=null && 0<=o.b && o.b<=255;
-	} catch (err){ 
-		isValid=false; 
-	} 
+		isValid =
+			Number.isInteger(o.x) && o.x != null && 0 <= o.x && o.x < DIM &&
+			Number.isInteger(o.y) && o.y != null && 0 <= o.y && o.y < DIM &&
+			Number.isInteger(o.r) && o.r != null && 0 <= o.r && o.r <= 255 &&
+			Number.isInteger(o.g) && o.g != null && 0 <= o.g && o.g <= 255 &&
+			Number.isInteger(o.b) && o.b != null && 0 <= o.b && o.b <= 255;
+	} catch (err) {
+		isValid = false;
+	}
 	return isValid;
 }
-wss.on('connection', function(ws) {
+wss.on('connection', function (ws) {
 	// heartbeat
-  	ws.isAlive = true;
-  	ws.on('pong', heartbeat);
+	ws.isAlive = true;
+	ws.on('pong', heartbeat);
 
 	// send initial board: this is slow!!!
-	for(x=0;x<dim;x++){
-		for(y=0;y<dim;y++){
-			var o = { 'x' : x, 'y' : y, 'r': board[x][y].r, 'g': board[x][y].g, 'b': board[x][y].b };
+	for (x = 0; x < DIM; x++) {
+		for (y = 0; y < DIM; y++) {
+			var o = { 'x': x, 'y': y, 'r': board[x][y].r, 'g': board[x][y].g, 'b': board[x][y].b };
 			ws.send(JSON.stringify(o));
 		}
 	}
 
 	// when we get a message from the client
-	ws.on('message', function(message) {
+	ws.on('message', function (message) {
 		console.log(message);
 		var o = JSON.parse(message);
-		if(isValidSet(o)){
+		if (isValidSet(o)) {
 			wss.broadcast(message);
 			board[o.x][o.y] = { 'r': o.r, 'g': o.g, 'b': o.b };
 		}
@@ -68,23 +74,25 @@ wss.on('connection', function(ws) {
 
 // heartbeat (ping) sent to all clients
 const interval = setInterval(function ping() {
-  wss.clients.forEach(function each(ws) {
-    if (ws.isAlive === false) return ws.terminate();
- 
-    ws.isAlive = false;
-    ws.ping(noop);
-  });
+	wss.clients.forEach(function each(ws) {
+		if (ws.isAlive === false) return ws.terminate();
+
+		ws.isAlive = false;
+		ws.ping(noop);
+	});
 }, 30000);
 
-// Static content
-var express = require('express');
-var app = express();
+// static client files
+if (app.get('env') === 'production') {
+	app.get("/", (req, res) => {
+		res.status(301).redirect("http://csc409-place-client.s3-website.ca-central-1.amazonaws.com")
+	})
+} else {
+	app.use('/', express.static('static_files'));
+}
 
-// static_files has all of statically returned content
-// https://expressjs.com/en/starter/static-files.html
-app.use('/',express.static('static_files')); // this directory has files to be returned
-
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(HTTP_PORT, function () {
+	console.log('mode: ' + app.get('env'))
+	console.log('Server listening on port ' + HTTP_PORT);
 });
 
