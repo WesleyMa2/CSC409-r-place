4e910bcd19708eb754acece7cc02f0f6c0fd5205
Joseph Sokolon <joseph.sokolon@mail.utoronto.ca>
2020-12-08 20:58:21 -0500

    Finished spark OKAYY

--- search/spark/search2.scala
+++ search/spark/search2.scala
@@ -14,22 +14,27 @@ def bruteIncrement(bruteOg: Brute, alphabetLen: Int, incrementBy: Long): Brute =
     return (brute._1, increment == 0)
 }
 
-def toAlpha(a: Array[Int], alphabet: String): Array[Char] = {
-    return a.map(c => alphabet(c))
-}
-def checkString(t: (Array[Char], Boolean), targetString: String): Boolean = {
-    return t._2 == true && t._1.mkString("") == targetString
+// Check if the brute is equal to the target string
+def checkString(t: (Array[Int], Boolean), targetString: String): Boolean = {
+    if (!t._2) return false
+    for (i <- 0 to targetString.length) {
+        if (targetString.charAt(i) != alphabet(t._1(i))) {
+            return false
+        }
+    }
+    return true
 }
 
-def checkRange(t: (Brute, Int), alphabetLen: Int): Boolean = {
+// Check all the strings between brute, and + length
+def checkRange(t: (Brute, Int), alphabetLen: Int, targetString: String, alphabet: String): Boolean = {
     var brute = t._1
     var i = 0
     while (i < t._2 && brute._2) {
-        val alpha = toAlpha(brute._1, alphabet)
-        if (checkString(alpha, targetString)) {
+        if (checkString((brute._1, brute._2), targetString)) {
             return true
         }
         brute = bruteIncrement(brute, alphabetLen, 1)
+        i = i + 1
     }
     return false
 }
@@ -48,57 +53,24 @@ def search(targetString: String, alphabet: String, threads: Int, workPerRound: I
     var roundState = (Array.fill(wordLen)(0), true)
     var iteration = 1
 
-    // setup work for each thread eg. threadState(n) = [([0,0], true), ([0,1], true), ...([z,z], false)]
-    // for (i <- threadState.indices){
-    //     var chunkStart = bruteIncrement(roundState, alphabetLen, i * workSize)
-    //     if (chunkStart._2 == true){
-    //         // println("Thread " + i + " start at " + i * workSize)
-    //         threadState(i) = sc.parallelize(Array.fill(workSize){BASIC_BRUTE}.zipWithIndex.map{case(_, c) => {bruteIncrement(chunkStart, alphabetLen, c)}})
-    //     } else {
-    //         // println("Thread " + i + " out of bounds")
-    //         threadState(i) = sc.parallelize(Array.fill(workSize)((Array(0), false)))
-    //     }
-    // }
     while(true) {
 
         // start each thread state at start of its chunk 
         threadState = threadState.zipWithIndex().map{case(el, c) => {(bruteIncrement(el._1, alphabetLen, c * workSize), el._2)}}
 
         // for each thread, iterate through its chunksize checking for matches
-        threadResults = threadState.map(el => checkRange(el, alphabetLen))
-
-        // setup work for each thread eg. threadState(n) = [([0,0], true), ([0,1], true), ...([z,z], false)]
-        // for (i <- threadState.indices){
-        //     var chunkStart = bruteIncrement(roundState, alphabetLen, i * workSize)
-        //     if (chunkStart._2 == true){
-        //         // println("Thread " + i + " start at " + i * workSize)
-        //         threadState(i) = threadState(i).zipWithIndex().map{case(_, c) => {bruteIncrement(chunkStart, alphabetLen, c)}}
-        //     } else {
-        //         // println("Thread " + i + " out of bounds")
-        //         threadState(i) = sc.parallelize(Array(BASIC_BRUTE))
-        //     }
-        // }
-
-        // for each thread, check if string matches, and save result
-        // for (i <- threadState.indices){
-        //     val matchedArr = threadState(i).map((t:Brute) => (toAlpha(t._1, alphabet), t._2)).map(t => checkString(t, targetString))
-        //     threadResults(i) = matchedArr.reduce(_ || _) == true
-        // }
+        threadResults = threadState.map(el => checkRange(el, alphabetLen, targetString, alphabet))
 
         println("Checked " + iteration * threads * workSize + " strings")
         // reduce all thread results and see if any are true, if so, break
-        if (threadResults.reduce(_ || _) == true) {
+        if (threadResults.treeReduce(_ || _) == true) {
             println("FOUND")
             return
         }
 
-        // // increase every thread state by workPerRound
-        // for (i <- threadState.indices){
-        //     threadState(i) = sc.parallelize(threadState(i).map((el:Brute) => bruteIncrement(el, alphabetLen, workSize * threads)))
-        // }
-
         // Go to next chunk aka increment entire array by iteration, break if out of bounds
         roundState = bruteIncrement(roundState, alphabetLen, workSize * threads)
+        threadState = sc.parallelize(Array.fill(threads)((roundState, workSize)))
         if (roundState._2 == false) {
             println("NOT FOUND")
             return
@@ -107,6 +79,6 @@ def search(targetString: String, alphabet: String, threads: Int, workPerRound: I
     }
 }
 
-val targetString = "this7"
+val targetString = "this7s"
 val alphabet = "abcdefghijklmnopqrstuvwxyz"
-spark.time(search(targetString, alphabet, 32, 1 << 22))
+spark.time(search(targetString, alphabet, 32, 1 << 24))
--- search/spark/shell.sh
+++
@@ -1,1 +0,0 @@
-docker exec -it spark_spark-master.1.$(docker service ps -f 'name=spark_spark-master.1' spark_spark-master -q --no-trunc | head -n1) /bin/bash -c "/shared/startShell.sh"
