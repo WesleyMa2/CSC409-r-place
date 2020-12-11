ed1871f365bc0b4093dc870fce9c6839cbcdc83f
Sokojoe <sokojoey@gmail.com>
2020-12-10 20:22:11 -0500

    consilidated start script

--- place/server/install_dependencies.sh
+++
@@ -1,4 +0,0 @@
-echo "Installing dependancies"
-curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
-sudo yum install nodejs
-npm install
--- place/server/start_server.sh
+++ place/server/start_server.sh
@@ -1,5 +1,9 @@
+echo "Installing dependencies"
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo bash -
+sudo yum install nodejs
+npm install
+sudo npm install pm2@latest -g
 echo "Redirecting ports"
 sudo iptables -t nat -I PREROUTING -p tcp --dport 80 -j REDIRECT --to-port 8080
 sudo iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
-pkill -f "server.js"
-NODE_ENV=production && node server.js &
+pm2 start npm -- run startprod
