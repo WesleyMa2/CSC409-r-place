4db6700081edef0bdade6da59ccc4c6b62d3e164
Wesley <weselykhma@gmail.com>
2020-12-10 19:18:29 -0500

    updated init scripts

--- place/server/initInstance.sh
+++ place/server/initInstance.sh
@@ -1,38 +1,4 @@
 #!/bin/bash
-# install_git_cmd=`aws ssm send-command \
-# 	--instance-ids "$1" \
-# 	--document-name "AWS-RunShellScript" \
-# 	--comment "IP config" \
-# 	--parameters commands="sudo yum update -y && sudo yum install git" \
-#     --query "Command.CommandId" \
-# 	--output text`
-# git_clone_cmd=`aws ssm send-command \
-# 	--instance-ids "$1" \
-# 	--document-name "AWS-RunShellScript" \
-# 	--comment "IP config" \
-# 	--parameters commands="cd && git clone https://mcsscm.utm.utoronto.ca:9999/git/409/20f/repo_a3group37.git" \
-#     --query "Command.CommandId" \
-# 	--output text`
-# aws ssm send-command \
-# 	--instance-ids "$1" \
-# 	--document-name "AWS-RunShellScript" \
-# 	--comment "IP config" \
-# 	--parameters commands="sudo yum update -y" \
-#     --query "Command.CommandId" \
-#     --region ""
-# 	--output text
-
-# echo $install_git_cmd
-# echo $git_clone_cmd
-# aws ssm list-command-invocations \
-# 	--command-id $install_git_cmd \
-# 	--details \
-#     --query "CommandInvocations[].CommandPlugins[].Output[]"
-# aws ssm list-command-invocations \
-# 	--command-id $git_clone_cmd \
-# 	--details \
-#     --query "CommandInvocations[].CommandPlugins[].Output[]"
-
 echo -n "Username: "
 read
 USERNAME=$REPLY
@@ -42,7 +8,7 @@ PASSWORD=$REPLY
 ssh -i "~/.ssh/place_server.pem" $1 \
         "sudo yum update -y && sudo yum install git && \
         cd && git clone https://$USERNAME:$PASSWORD@mcsscm.utm.utoronto.ca:9999/git/409/20f/repo_a3group37.git &&\
-        cd /repo_a3group37/place/server && \
+        cd ~/repo_a3group37/place/server && chmod 700 *\
         ./install_dependencies.sh && \
         ./start_server.sh
         "
--- place/server/start_server.sh
+++ place/server/start_server.sh
@@ -1,2 +1,2 @@
-iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
-npm run startprod &
+sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 80 -j REDIRECT --to-port 8080
+NODE_ENV=production && node server.js &
