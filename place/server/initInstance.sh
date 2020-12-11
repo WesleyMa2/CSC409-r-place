7ae63060ee17198a92c888304ecdd9ceebbd6298
Wesley <weselykhma@gmail.com>
2020-12-10 20:33:11 -0500

    fixed initInstance

--- place/server/initInstance.sh
+++ place/server/initInstance.sh
@@ -10,6 +10,5 @@ ssh -i "~/.ssh/place_server.pem" ec2-user@$1 \
         cd && rm -r -f repo_a3group37/ && \
         git clone https://$USERNAME:$PASSWORD@mcsscm.utm.utoronto.ca:9999/git/409/20f/repo_a3group37.git && \
         cd ~/repo_a3group37/place/server && chmod 700 * &&\
-        ./install_dependencies.sh && \
         ./start_server.sh
         "
