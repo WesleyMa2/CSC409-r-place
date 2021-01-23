cf8c97b2f1ce34176c084c1796a87c5768285a2e
Wesley <weselykhma@gmail.com>
2021-01-23 17:28:33 -0500

    added a screenshot

--- place/server/ec2_shell.sh
+++ place/server/ec2_shell.sh
@@ -1,2 +1,3 @@
 # make sure you have .ssh/place_server.pem 
-ssh -i "~/.ssh/place_server.pem" ec2-user@ec2-54-234-48-204.compute-1.amazonaws.com 
+ssh -i "~/.ssh/place_server.pem" ec2-user@ec2-54-224-18-55.compute-1.amazonaws.com
+
